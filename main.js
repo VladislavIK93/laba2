e1c1479e0c67d9293098499328fae14c81d9be21
VladislavIK93 <johnnyproduction023@gmail.com>
2021-03-11 00:09:11 +0200

    Laba2_FinalEdition

---
+++ main.js
@@ -0,0 +1,8 @@
+const index = require('./node_modules/math/index.js');
+
+	console.log(index.abs(-5))
+let matrix = [0, -100, 200, -6]
+	console.log(index.min(matrix))
+	console.log(index.summ(10,-10))
+
+
